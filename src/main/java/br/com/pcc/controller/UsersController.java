032aec0107c6bdda7d2d5f4828dcbcae5fee3161
Gtosta96 <gabrieltosta3@gmail.com>
2016-02-29 23:49:31 -0300

    adicionado spring mvc

--- src/main/java/br/com/pcc/controller/UsersController.java
+++ src/main/java/br/com/pcc/controller/UsersController.java
@@ -1,5 +1,18 @@
 package br.com.pcc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class UsersController {
 
+	@RequestMapping(value = "/authLogin", method = RequestMethod.GET, produces = "application/json")
+	public String authLogin(@RequestParam("username") String username, @RequestParam("password") String password)
+	{
+		
+		System.out.println("entrou");
+		return "Work it!";
+	}
 }
