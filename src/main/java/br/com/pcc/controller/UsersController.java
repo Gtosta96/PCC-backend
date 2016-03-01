951fd67c41a161e37de71324e3d81991915488c9
Gtosta96 <gabrieltosta3@gmail.com>
2016-03-01 17:17:03 -0300

    Evoluido conexão com frontend

--- src/main/java/br/com/pcc/controller/UsersController.java
+++ src/main/java/br/com/pcc/controller/UsersController.java
@@ -4,15 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import br.com.pcc.entity.UsersEntity;
 
 @Controller
 public class UsersController {
 
-	@RequestMapping(value = "/authLogin", method = RequestMethod.GET, produces = "application/json")
-	public String authLogin(@RequestParam("username") String username, @RequestParam("password") String password)
-	{
+	@RequestMapping(value = "/loginAuth", method = RequestMethod.GET)
+	@ResponseBody
+	public UsersEntity authLogin(@RequestParam(name = "username", required = false) String username,
+								 @RequestParam(name = "password", required = false) String password) {
 		
-		System.out.println("entrou");
-		return "Work it!";
+		UsersEntity user = new UsersEntity();
+		
+		System.out.println(username);
+		System.out.println(password);
+		
+		user.setUsername("USUARIO1");
+		user.setPassword("USUARIO1");
+		user.setEmail("USUARIO1");
+
+		return user;
 	}
 }
